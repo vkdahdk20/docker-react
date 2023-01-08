f6eb41600b38607ac407f101ddf28383e41a9238
Henry <vkdahdk@gmail.com>
2023-01-08 22:21:37 +0900

    travis 333

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React 2223
+          Learn React 222322
         </a>
       </header>
     </div>
